42d1c090773b52802f26c2a0b69075264d3d15cd
Steven Rand <srand@palantir.com>
2018-05-01 13:46:33 +1000

    add publishing code and changelog for 2.9.1-palantir.1

---
+++ palantir/CHANGELOG.md
@@ -0,0 +1,7 @@
+# 2.9.1-palantir.1
+
+Based on Apache Hadoop tag `release-2.9.1-RC0`.
+
+* Revert [HADOOP-13188](https://issues.apache.org/jira/browse/HADOOP-13188)
+* Backport [HADOOP-14652](https://issues.apache.org/jira/browse/HADOOP-14652)
+* Upgrade the AWS Java SDK from 1.11.199 to 1.11.201
---
+++ palantir/publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+version=$(git describe --tags --always)
+file_name="hadoop-dist-${version}.tgz"
+
+tmp_settings="tmp-settings.xml"
+echo "<settings><servers><server>" > $tmp_settings
+echo "<id>bintray-palantir-release</id><username>$BINTRAY_USERNAME</username>" >> $tmp_settings
+echo "<password>$BINTRAY_PASSWORD</password>" >> $tmp_settings
+echo "</server></servers></settings>" >> $tmp_settings
+
+# Deploy JARs to Bintray
+mvn -e versions:set -DnewVersion=$version
+mvn -e source:jar install -DskipTests -Dmaven.javadoc.skip=true
+mvn -e --settings $tmp_settings source:jar -DskipTests deploy
+
+# Publish a dist to Bintray
+mvn -e package -Pdist,native,src -DskipTests -Dmaven.javadoc.skip=true -Dtar
+curl -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD -T hadoop-dist/target/hadoop-${version}.tar.gz "https://api.bintray.com/content/palantir/releases/hadoop/${version}/org/apache/hadoop/hadoop-dist/${version}/${file_name}"
+
+# Tell Bintray to publish the artifacts for this release
+curl -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD -X POST https://api.bintray.com/content/palantir/releases/hadoop/$(git describe --tags)/publish
