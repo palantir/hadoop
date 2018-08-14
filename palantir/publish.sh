2546d06c5ab8782e87fc3a6ede256541d9931cab
Steven Rand <srand@palantir.com>
2018-08-14 21:48:00 +1000

    appease the build gods, maybe

--- palantir/publish.sh
+++ palantir/publish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+# Circle doesn't set this for us, but our mvn package step needs it due to its use of ${env.JAVA_HOME}.
+export JAVA_HOME=/usr/lib/jvm/jdk1.8.0
+
 version=$(git describe --tags --always)
 file_name="hadoop-dist-${version}.tgz"
 
@@ -15,7 +18,7 @@ mvn -e versions:set -DnewVersion="$version"
 mvn -e --settings $tmp_settings source:jar -DskipTests deploy
 
 # Publish a dist to Bintray
-mvn -e package -Pdist,native,src -DskipTests -Dmaven.javadoc.skip=true -Dtar
+mvn -e package -Pdist,native,src -DskipTests -Dmaven.javadoc.skip=true -Dtar | grep -v 'longer than 100'
 curl -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD -T hadoop-dist/target/hadoop-${version}.tar.gz "https://api.bintray.com/content/palantir/releases/hadoop/${version}/org/apache/hadoop/hadoop-dist/${version}/${file_name}"
 
 # Tell Bintray to publish the artifacts for this release
