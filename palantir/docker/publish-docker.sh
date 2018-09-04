691639bbe2f96404bac922ae65cc3e3833fea545
Steven Rand <srand@palantir.com>
2018-09-04 14:10:20 +1000

    whoops

--- palantir/docker/publish-docker.sh
+++ palantir/docker/publish-docker.sh
@@ -3,6 +3,9 @@ set -eu
 
 IMAGE_NAME=palantirtechnologies/circle-hadoop-2
 
+echo $PWD
+cd palantir/docker
+echo $PWD
 docker build . -t ${IMAGE_NAME}
 docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"
 docker push $IMAGE_NAME
