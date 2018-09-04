ac1721d4b9a770aa1198bc2e8d4302f62dfed554
Steven Rand <srand@palantir.com>
2018-09-04 14:45:29 +1000

    tag docker image as hadoop-2

--- palantir/docker/publish-docker.sh
+++ palantir/docker/publish-docker.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -eu
 
-IMAGE_NAME=palantirtechnologies/circle-hadoop-2
+IMAGE_NAME=palantirtechnologies/circle-hadoop
 
 echo $PWD
 cd palantir/docker
 echo $PWD
 docker build . -t ${IMAGE_NAME}
+docker tag $IMAGE_NAME hub.docker.com/$IMAGE_NAME:hadoop-2
 docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"
 docker push $IMAGE_NAME
