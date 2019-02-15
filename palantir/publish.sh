3fd19703339cd0bacb454bd1db8d6fbbfe75611e
Steven Rand <srand@palantir.com>
2019-02-15 12:41:48 +1100

    less spammy output

--- palantir/publish.sh
+++ palantir/publish.sh
@@ -11,15 +11,15 @@ echo "<password>$BINTRAY_PASSWORD</password>" >> $tmp_settings
 echo "</server></servers></settings>" >> $tmp_settings
 
 # Update the version.
-mvn -e versions:set -DnewVersion="$version"
+mvn -e versions:set -DnewVersion="$version" | grep -v 'Progress'
 echo $PWD
 sed -i "s/<hadoop\.version>3\.2\.0/<hadoop\.version>$version/g" pom.xml
 
 # Deploy JARs to Bintray
-mvn -e --settings $tmp_settings source:jar -DskipTests deploy
+mvn -e --settings $tmp_settings source:jar -DskipTests deploy | grep -v 'Progress'
 
 # Publish a dist to Bintray
-mvn -e package -Pdist,native,src -DskipTests -Dmaven.javadoc.skip=true -Dtar
+mvn -e package -Pdist,native,src -DskipTests -Dmaven.javadoc.skip=true -Dtar | grep -v 'Progress'
 curl -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD -T hadoop-dist/target/hadoop-${version}.tar.gz "https://api.bintray.com/content/palantir/releases/hadoop/${version}/org/apache/hadoop/hadoop-dist/${version}/${file_name}"
 
 # Tell Bintray to publish the artifacts for this release
