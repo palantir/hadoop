8c1f836728c7d2e7946a40735d7422fb509f881e
Justin Uang <juang@palantir.com>
2019-02-18 12:02:28 -0500

    fix

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/DefaultS3ClientFactory.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/DefaultS3ClientFactory.java
@@ -24,7 +24,6 @@ import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.S3ClientOptions;
-import com.amazonaws.thirdparty.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.VersionInfo;
@@ -63,11 +62,6 @@ public class DefaultS3ClientFactory extends Configured implements
    */
   public static ClientConfiguration createAwsConf(Configuration conf) {
     final ClientConfiguration awsConf = new ClientConfiguration();
-
-    if (conf.get() != null) {
-      awsConf.getApacheHttpClientConfig().setSslSocketFactory(ConnectionSocketFactory);
-    }
-
     initConnectionSettings(conf, awsConf);
     initProxySupport(conf, awsConf);
     initUserAgent(conf, awsConf);
